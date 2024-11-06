e5642029f599fe48d06bc13c7efaaee1686e2e93
David <dsanzraya@hotmail.com>
2024-11-06 10:32:55 +0100

    usings

--- SQLEmployeesConnection/Scripts/DBConnect.cs
+++ SQLEmployeesConnection/Scripts/DBConnect.cs
@@ -1,11 +1,5 @@
-﻿using SQLEmployeesConnection.Scripts;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data;
 
 namespace SQLEmployeesConnection
